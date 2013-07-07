434353e962afe4637464227851bc4293834a81c4
Rufus Pollock <rufus.pollock@okfn.org>
2013-07-07 01:19:15 +0100

    [cors][s]: fix up OPTIONS for CORS.

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ var CORSSupport = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
 }
 
 app.configure(function(){
