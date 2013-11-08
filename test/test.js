18b74ca8d330f60d95b38db7c8ace7a237f3989c
Andy Lulham <a.lulham@gmail.com>
2013-11-08 10:30:03 +0000

    Couple more basic tests
    Refs #29.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,9 @@
 var app = require('../app.js').app;
 var request = require('supertest')(app);
+var assert = require('assert');
 var _ = require('underscore');
+
+var data_url = 'https://raw.github.com/okfn/datapipes/master/test/data/gla.csv';
 
 describe('GET /', function(){
   it('should respond with homepage', function(done){
@@ -20,5 +23,41 @@ _.each(ops, function(op) {
         .expect('Content-Type', /html/)
         .expect(200, done);
     });
+  });
+});
+
+describe('GET /csv/head/html', function(){
+  it('should by default return 10 rows', function(done){
+    url = '/csv/head/html?url=' + data_url;
+    request
+      .get(url)
+      .expect('Content-Type', /html/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) done(err);
+
+        var out = res.text;
+        numRows = out.match(/<tr/g).length;
+        assert.equal(numRows, 10);
+
+        done();
+      });
+  });
+
+  it('should return 5 rows', function(done){
+    url = '/csv/head -n 5/html?url=' + data_url;
+    request
+      .get(url)
+      .expect('Content-Type', /html/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) done(err);
+
+        var out = res.text;
+        numRows = out.match(/<tr/g).length;
+        assert.equal(numRows, 5);
+
+        done();
+      });
   });
 });
