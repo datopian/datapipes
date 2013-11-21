datopian/datapipes

commit 03b81d1cdab6c2daa2e3b2f0cec30f0921b2f1f5

Remove -H option from html operator

lib/operators.js
@@ -205,7 +205,6 @@ var operators = {
         p: {alias: 'escapechar', default: '\\', string: true},
         q: {alias: 'quotechar', default: '\"', string: true},
         t: {alias: 'tabs', boolean: true},
-        H: {alias: 'no-header-row', boolean: true},
         S: {alias: 'skipinitialspace', boolean: true},
       })
       .parse(args)
@@ -225,18 +224,6 @@ var operators = {
   // return our HTML transformation
   outhtml: function(args) {
     Transform.call(this, {objectMode: true});
-
-    var argv = optimist()
-      .options({
-        H: {alias: 'no-header-row', boolean: true},
-      })
-      .parse(args)
-    ;
-
-    this._stylefirstrow = true;
-    if (!headerrow && (argv.H || argv['header-row'] === false)) {
-      this._stylefirstrow = false;
-    }
 
     this._initialHtmlSent = false;
   },
@@ -469,7 +456,7 @@ operators.outhtml.prototype._transform = function(chunk, encoding, done) {
     this.push('<body class="view-html">');
     this.push('<table>');
     this.push('<thead>');
-    if (this._stylefirstrow) {
+    if (this.headerrow) {
       this.push('<tr><th class="counter"></th>');
       json.row.forEach(function(item) {
         self.push('<th title="%s">%s</th>'.replace(/%s/g, item));

commit db78bf1136a176075e52805ba80967fc9de66eef

Fix bug introduced in 03b81d1c

lib/operators.js
@@ -436,7 +436,7 @@ operators.outhtml.prototype._transform = function(chunk, encoding, done) {
     this.push('<body class="view-html">');
     this.push('<table>');
     this.push('<thead>');
-    if (this.headerrow) {
+    if (headerrow) {
       this.push('<tr><th class="counter"></th>');
       json.row.forEach(function(item) {
         self.push('<th title="%s">%s</th>'.replace(/%s/g, item));