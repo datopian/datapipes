907ae44e34cdd9126daa389f6898cbce98965b5f
Rufus Pollock <rufus.pollock@okfn.org>
2014-03-09 17:20:31 +0000

    [#75,wizard][s]: support for choosing output format.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ var fs = require('fs')
   ;
 
 exports.wizard = function(req, res) {
-  var pipeline = '/csv/';
   if (!req.query.url) {
     res.render('interactive.html', tmplData);
     return;
@@ -14,14 +13,24 @@ exports.wizard = function(req, res) {
   queryStr = '?url=' + req.query.url;
   var tmp = _.extend({}, req.query);
   delete tmp.url;
-  var pipes = '/csv/' + _.map(tmp, function(v, k) {
-    return k + ' ' + v
-  }).join('/');
+  var output = req.query.output || 'html';
+  delete tmp.output;
 
-  var pipeline = req.protocol + '://' + req.get('host') + pipes + '/' + queryStr;
+  var pipes = ['csv'];
+  pipes = pipes.concat(_.map(tmp, function(v, k) {
+    return k + ' ' + v
+  }));
+  // csv is default output format
+  if (output !== 'csv') {
+    pipes.push(output);
+  }
+  pipes = pipes.join('/');
+
+  var pipeline = req.protocol + '://' + req.get('host') + '/' + pipes + '/' + queryStr;
 
   var tmplData = {
     url: req.query.url,
+    output: output,
     pipeline: pipeline,
     query: tmp
   };
